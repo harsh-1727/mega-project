4a18a9fa7a62c4d01fb17365bbd7fa0d6a05f380
harsh-1727 <142785417+harsh-1727@users.noreply.github.com>
2025-01-28 09:24:40 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+# THIS IS NOT INTEGRATING WITH THE MAIN PAGE BECAUSE OF API KEY
+
 from openai import OpenAI
 client = OpenAI(
     api_key="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"
